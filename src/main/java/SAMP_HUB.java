650ab00fcfaa43bbe6fac5e708a29e953f7eab41
haigron <regis.haigron@obspm.fr>
2023-10-13 10:48:36 +0200

    try compression

--- src/main/java/SAMP_HUB.java
+++ src/main/java/SAMP_HUB.java
@@ -6,8 +6,6 @@
  *     http://creativecommons.org/publicdomain/zero/1.0/
  */
 
-
-
 import ij.IJ;
 import ij.ImageJ;
 import ij.ImagePlus;
@@ -33,161 +31,155 @@ import org.padc.*;
  */
 public class SAMP_HUB implements PlugIn {
     static HubConnector conn = null;
-    
-    public void runHub(ClientProfile profile){                
-        if (!profile.isHubRunning()){
+
+    public void runHub(ClientProfile profile) {
+        if (!profile.isHubRunning()) {
             try {
                 System.out.println("start hub");
                 Hub.runHub(HubServiceMode.MESSAGE_GUI);
             } catch (IOException ex) {
                 Logger.getLogger(SAMP_HUB.class.getName()).log(Level.SEVERE, null, ex);
             }
-        }        
+        }
     }
-    
-    public String getUrl(String uri){
+
+    public String getUrl(String uri) {
         URL url;
-        String contentType =null;
-        
-        if ( uri.contains("http://") || uri.contains("https://")){
+        String contentType = null;
+
+        if (uri.contains("http://") || uri.contains("https://")) {
             try {
                 url = new URL(uri);
-                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 connection.setRequestMethod("HEAD");
-                connection.connect();                
+                connection.connect();
                 int status = connection.getResponseCode();
                 if (status == HttpURLConnection.HTTP_MOVED_TEMP
                         || status == HttpURLConnection.HTTP_MOVED_PERM
-                            || status == HttpURLConnection.HTTP_SEE_OTHER){
+                        || status == HttpURLConnection.HTTP_SEE_OTHER) {
                     // get redirect url from "location" header field
-                    String newUrl = connection.getHeaderField("Location");                                
+                    String newUrl = connection.getHeaderField("Location");
                     // open the new connnection again
-                    System.out.println("Redirect to URL : " + newUrl);                    
+                    System.out.println("Redirect to URL : " + newUrl);
                     return getUrl(newUrl);
-                }                
-                
-            } 
-            catch (MalformedURLException ex) {
-                System.out.println("Invalid "+uri);
+                }
+
+            } catch (MalformedURLException ex) {
+                System.out.println("Invalid " + uri);
                 ex.printStackTrace();
-            } 
-            catch (IOException ex) {
-                System.out.println("Cant open "+uri);
+            } catch (IOException ex) {
+                System.out.println("Cant open " + uri);
                 ex.printStackTrace();
             }
         }
         return uri;
     }
-    
+
     /**
      * @param uri
      * @return
      */
-    public String getType(String uri){
+    public String getType(String uri) {
         URL url;
-        String contentType =null;
-        
-        if ( uri.contains("http://") || uri.contains("https://")){
+        String contentType = null;
+
+        if (uri.contains("http://") || uri.contains("https://")) {
             try {
-                System.out.println("uri is url "+uri);
+                System.out.println("uri is url " + uri);
                 url = new URL(uri);
-                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 connection.setRequestMethod("HEAD");
-                connection.connect();                
+                connection.connect();
                 int status = connection.getResponseCode();
-                if(status == 404){
+                if (status == 404) {
                     IJ.showMessage("SAMP HUB",
-                    "Url:"+uri+" not found");
-                }
-                else{
+                            "Url:" + uri + " not found");
+                } else {
                     contentType = connection.getContentType();
-                    if ( contentType == null){
-                        System.out.println("WARNING : no content type for url "+uri);
-                        if ( uri.endsWith(".fits")){
-                            contentType="image/fits";
+                    if (contentType == null) {
+                        System.out.println("WARNING : no content type for url " + uri);
+                        if (uri.endsWith(".fits")) {
+                            contentType = "image/fits";
                         }
                     }
                 }
-                
-            } 
-            catch (MalformedURLException ex) {
-                System.out.println("Invalid "+uri);
+
+            } catch (MalformedURLException ex) {
+                System.out.println("Invalid " + uri);
                 ex.printStackTrace();
-            } 
-            catch (IOException ex) {
-                System.out.println("Cant open "+uri);
+            } catch (IOException ex) {
+                System.out.println("Cant open " + uri);
                 ex.printStackTrace();
             }
         }
         return contentType;
     }
-            
+
     @Override
-    public void run(String args){              
-        //showAbout();
+    public void run(String args) {
+        // showAbout();
         ClientProfile profile = DefaultClientProfile.getProfile();
-        
+
         runHub(profile);
-        if (conn == null){            
+        if (conn == null) {
             conn = new HubConnector(profile);
-        }        
-        if (!conn.isConnected()){
-        
+        }
+        if (!conn.isConnected()) {
+
             Metadata meta = new Metadata();
             meta.setName("IMAGE-J");
             meta.setDescriptionText("Image J");
             conn.declareMetadata(meta);
             conn.addMessageHandler(new AbstractMessageHandler("image.load.fits") {
                 public Map processCall(HubConnection c, String senderId, Message msg) {
-                    // 
+                    //
                     Map params = msg.getParams();
                     String msg_url = params.get("url").toString();
                     String url = getUrl(msg_url);
                     String type = getType(url);
-                        
-                    System.out.println("type |"+type+"|");                    
-                    if ( url.startsWith("file://localhost", 0) || (type != null && "image/fits".equals(type))){
-                            String[] parts = url.split("/");
-                            ImagePlus image;
-                            try {
-                                String name = parts[parts.length-1];                                     
-                                int index=url.lastIndexOf('/');
-                                String path = url.substring(0,index)+"/";
-                                image = (ImagePlus)new FitsReader(path,parts[parts.length-1]);
-                                image.show();                        
-                            } catch (IOException ex) {
-                                
-                                //Logger.getLogger(SAMP_HUB.class.getName()).log(Level.SEVERE, null, ex);
-                                System.out.println("try to open url "+url);
-                                IJ.debugMode=true;
-                                image = IJ.openImage(url);
-                                image.show();
-                            }
-                    }
-                    else{
+
+                    System.out.println("type |" + type + "|");
+                    if (url.startsWith("file://localhost", 0) || (type != null && "image/fits".equals(type)) ||
+                            url.endsWith(".fits")) {
+                        String[] parts = url.split("/");
+                        ImagePlus image;
+                        try {
+                            String name = parts[parts.length - 1];
+                            int index = url.lastIndexOf('/');
+                            String path = url.substring(0, index) + "/";
+                            image = (ImagePlus) new FitsReader(path, parts[parts.length - 1]);
+                            image.show();
+                        } catch (IOException ex) {
+
+                            // Logger.getLogger(SAMP_HUB.class.getName()).log(Level.SEVERE, null, ex);
+                            System.out.println("try to open url " + url);
+                            IJ.debugMode = true;
+                            image = IJ.openImage(url);
+                            image.show();
+                        }
+                    } else {
                         System.out.println("try to open url");
-                        IJ.debugMode=true;
+                        IJ.debugMode = true;
                         ImagePlus image = IJ.openImage(url);
                         image.show();
-                    }                    
+                    }
                     return null;// do stuff
                 }
             });
             Subscriptions subs = conn.computeSubscriptions();
-            subs.addMType("image.load.fits");           
-            conn.declareSubscriptions(subs);   
+            subs.addMType("image.load.fits");
+            conn.declareSubscriptions(subs);
             try {
                 conn.getConnection();
             } catch (SampException ex) {
                 Logger.getLogger(SAMP_HUB.class.getName()).log(Level.SEVERE, null, ex);
-            }            
-        }        
+            }
+        }
     }
 
     public void showAbout() {
-            IJ.showMessage("SAMP HUB",
-                "hub to use SAMP protocol"
-            );
+        IJ.showMessage("SAMP HUB",
+                "hub to use SAMP protocol");
     }
 
     /**
@@ -198,33 +190,36 @@ public class SAMP_HUB implements PlugIn {
      *
      * @param args unused
      */
-    
-    public static final void testUrl(String url) throws IOException{
+
+    public static final void testUrl(String url) throws IOException {
         int index = url.lastIndexOf('/');
-        String path = url.substring(0,index)+"/";
-        String name = url.substring(index+1);
-        System.out.println("path "+path+" "+name);
+        String path = url.substring(0, index) + "/";
+        String name = url.substring(index + 1);
+        System.out.println("path " + path + " " + name);
         ImagePlus image = new FitsReader(path, name);
         image.show();
     }
-    
-    public static void main(String[] args) throws IOException {
-            // set the plugins.dir property to make the plugin appear in the Plugins menu
-            Class<?> clazz = SAMP_HUB.class;
-            String url = clazz.getResource("/" + clazz.getName().replace('.', '/') + ".class").toString();
-            String pluginsDir = url.substring("file:".length(), url.length() - clazz.getName().length() - ".class".length());
-            System.setProperty("plugins.dir", pluginsDir);
 
-            // start ImageJ
-            new ImageJ();
+    public static void main(String[] args) throws IOException {
+        // set the plugins.dir property to make the plugin appear in the Plugins menu
+        Class<?> clazz = SAMP_HUB.class;
+        String url = clazz.getResource("/" + clazz.getName().replace('.', '/') + ".class").toString();
+        String pluginsDir = url.substring("file:".length(),
+                url.length() - clazz.getName().length() - ".class".length());
+        System.setProperty("plugins.dir", pluginsDir);
+
+        // start ImageJ
+        new ImageJ();
 
         testUrl("http://voparis-srv.obspm.fr/vo/planeto/apis/dataset/Bastet/Uranus_-_2011_09-29_Nov/obrx18hbq_x2d.fits");
-        
-        //image = (ImagePlus)new FitsReader("file://localhost/home/haigron/.aladin/.aladin.33916/","sampBj%28IIIa-J.GG395%29.ivo%3A..vopdc.obspm1500650771926.fits");
-        //ImagePlus image = new FitsReader("http://climso.irap.omp.eu/data/files/epntap/","imoa_03933_l2_20170727_16212700_emi1.fts.bz2");           
-        //    image.show();
-            
-            // run the plugin
-            //IJ.runPlugIn(clazz.getName(), "");
+
+        // image = (ImagePlus)new
+        // FitsReader("file://localhost/home/haigron/.aladin/.aladin.33916/","sampBj%28IIIa-J.GG395%29.ivo%3A..vopdc.obspm1500650771926.fits");
+        // ImagePlus image = new
+        // FitsReader("http://climso.irap.omp.eu/data/files/epntap/","imoa_03933_l2_20170727_16212700_emi1.fts.bz2");
+        // image.show();
+
+        // run the plugin
+        // IJ.runPlugIn(clazz.getName(), "");
     }
 }
--- src/test/java/org/padc/FitsReaderJUnitTest.java
+++ src/test/java/org/padc/FitsReaderJUnitTest.java
@@ -20,42 +20,44 @@ import static org.junit.Assert.*;
  * @author haigron
  */
 public class FitsReaderJUnitTest {
-    
+
     public FitsReaderJUnitTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
-    /*
+
     @Test
-     public void testFitsReader() throws IOException {
-         ImagePlus f = new FitsReader("http://voparis-srv.obspm.fr/vo/planeto/apis/dataset/Bastet/Saturn_-_2012_28_Mar-06_Apr/","jbswi4h8q_drz.fits");         
-         Assert.assertNotNull("image plus not null", f);
-     }
-          
-     
-     @Test
-     public void testFitsReaderBz2() throws IOException {
-         ImagePlus f = new FitsReader("http://climso.irap.omp.eu/data/files/epntap/","imoa_03933_l2_20170727_16212700_emi1.fts.bz2");         
-         Assert.assertNotNull("image plus not null", f);
-     }
+    public void testFitsReader() throws IOException {
+        ImagePlus f = new FitsReader(
+                "https://idoc-medoc.ias.u-psud.fr/sitools/datastorage/user/archive_SDO_DEM/2023/10/DEM_aia_2023-10-03T23_35_images/",
+                "DEM_aia_2023-10-03T23_35_width.fits");
+        Assert.assertNotNull("image plus not null", f);
+    }
+    /*
+     * 
+     * @Test
+     * public void testFitsReaderBz2() throws IOException {
+     * ImagePlus f = new FitsReader("http://climso.irap.omp.eu/data/files/epntap/",
+     * "imoa_03933_l2_20170727_16212700_emi1.fts.bz2");
+     * Assert.assertNotNull("image plus not null", f);
+     * }
      */
-     
-     
+
 }
